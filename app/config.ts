5017344530e8d933b26a24c3785cec1591b3467e
Chris <chris35469@gmail.com>
2024-03-12 15:55:16 -0500

    Update confid URL links

--- app/config.ts
+++ app/config.ts
@@ -2,8 +2,8 @@ import { base } from 'viem/chains';
 
 
 // use NODE_ENV to not have to change config based on where it's deployed https://zizzamia.xyz
-export const NEXT_PUBLIC_URL = process.env.NODE_ENV == "development" ? 'https://5111-2600-1700-9cc0-3ab0-58f4-c735-8b5c-4d94.ngrok-free.app' : 'https://5111-2600-1700-9cc0-3ab0-58f4-c735-8b5c-4d94.ngrok-free.app';
-export const API_URL = process.env.NODE_ENV == "development" ? 'https://5111-2600-1700-9cc0-3ab0-58f4-c735-8b5c-4d94.ngrok-free.app' : 'https://5111-2600-1700-9cc0-3ab0-58f4-c735-8b5c-4d94.ngrok-free.app';
+export const NEXT_PUBLIC_URL = process.env.NODE_ENV == "development" ? 'https://5111-2600-1700-9cc0-3ab0-58f4-c735-8b5c-4d94.ngrok-free.app' : 'https://w3bbie-gm.netlify.app';
+export const API_URL = process.env.NODE_ENV == "development" ? 'https://5111-2600-1700-9cc0-3ab0-58f4-c735-8b5c-4d94.ngrok-free.app' : 'https://w3bbie-gm.netlify.app';
 
 export const CHAIN = base;
 export const CONTRACT_ADDRESS = '0xfac759cefe113e510d495e8ebefa1fa7b1c7b338';
